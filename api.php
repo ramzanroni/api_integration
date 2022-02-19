f1382a15f229d2cdcdb5a442ffb2bbd8d1ab730b
ramzan <mdramzanroni76@gmail.com>
2022-02-20 01:10:08 +0600

    apiIntegration

--- api.php
+++ api.php
@@ -3,12 +3,15 @@
 // post method
 
 $post = array(  //data array from user side
-	"id" => 7,
-	"user_name" => "user_name",
-	"user_pass" => "user_pass",
-	"user_email"   => "user_email",
-	"user_group_id"   => "user_group_id",
-	"previlege"   => 5,
+
+	"key"=>"value",
+	"key"=>"value",
+	"key"=>"value",
+	"key"=>"value",
+	"key"=>"value",
+	"key"=>"value",
+	"key"=>"value",
+	
 );
 $data = json_encode($post); // json encoded
 
@@ -28,11 +31,13 @@ curl_close ($ch);
 print_r($server_output);
 
 
+
+
 // get method api
 	
 $sendernumber="";  //
 
-    $url = "http://sms.viatech.com.bd/smsapi?api_key=key&type=text&contacts=".$sendernumber."&senderid=".$sender_id."&msg=".$mysms; //this is url
+    $url = ; //url will be here
     $ch = curl_init();
     curl_setopt($ch, CURLOPT_URL, $url);
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
